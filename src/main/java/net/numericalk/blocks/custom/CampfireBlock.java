abfd6ff166a4329fcb957145cc25f1b952e4d657
Fungus Defender <capybara.okipullup121@gmail.com>
2025-06-14 14:57:08 +0700

    Alpha

--- src/main/java/net/numericalk/blocks/custom/CampfireBlock.java
+++ src/main/java/net/numericalk/blocks/custom/CampfireBlock.java
@@ -6,19 +6,20 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.IntProperty;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.ItemScatterer;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.*;
 import net.minecraft.util.function.BooleanBiFunction;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
@@ -26,12 +27,14 @@ import net.minecraft.world.World;
 import net.numericalk.blocks.entity.SnailBlockEntities;
 import net.numericalk.blocks.entity.custom.CampfireBlockEntity;
 import net.numericalk.datagen.SnailItemTagsProvider;
+import net.numericalk.items.SnailItems;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.stream.Stream;
 
-//CHANGE TO BLOCK ENTITY
 public class CampfireBlock extends BlockWithEntity implements BlockEntityProvider{
+
+    public static final EnumProperty<Direction> FACING = Properties.HORIZONTAL_FACING;
 
     //1 Bark
     //2 One Stick
@@ -47,6 +50,37 @@ public class CampfireBlock extends BlockWithEntity implements BlockEntityProvide
     //4 Large Fire
     public static final IntProperty LIT = IntProperty.of("lit", 1, 4);
 
+    //1 No Stick
+    //2 One Stick
+    //3 Two Sticks
+    //4 Three Sticks
+    public static final IntProperty COOKING = IntProperty.of("cooking", 1, 4);
+
+    @Override
+    public @Nullable BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+    }
+
+    @Override
+    protected BlockState rotate(BlockState state, BlockRotation rotation) {
+        return state.with(FACING, rotation.rotate(state.get(FACING)));
+    }
+
+    @Override
+    protected BlockState mirror(BlockState state, BlockMirror mirror) {
+        return state.rotate(mirror.getRotation(state.get(FACING)));
+    }
+
+    public static int getLuminance(BlockState state) {
+        return switch (state.get(LIT)){
+            case 1 -> 0;
+            case 2 -> 7;
+            case 3 -> 11;
+            case 4 -> 15;
+            default -> 7;
+        };
+    }
+
     private static final VoxelShape SHAPE_BASE = Stream.of(
             Block.createCuboidShape(6, 0, 3, 10, 1, 13),
             Block.createCuboidShape(10, 0, 4, 12, 1, 12),
@@ -54,7 +88,40 @@ public class CampfireBlock extends BlockWithEntity implements BlockEntityProvide
             Block.createCuboidShape(4, 0, 4, 6, 1, 12),
             Block.createCuboidShape(12, 0, 6, 13, 1, 10)
     ).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, BooleanBiFunction.OR)).get();
-    private static final VoxelShape SHAPE_FULL = Block.createCuboidShape(2, 0, 2, 13, 10, 13);
+    private static final VoxelShape SHAPE_FULL =
+            Block.createCuboidShape(2, 0, 2, 13, 10, 13);
+    private static final VoxelShape SHAPE_FULL_1_WEST = Stream.of(
+            Block.createCuboidShape(7, 0, 0, 9, 16, 1),
+            Block.createCuboidShape(2, 0, 2, 13, 10, 13)
+    ).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, BooleanBiFunction.OR)).get();
+    private static final VoxelShape SHAPE_FULL_2_WEST = Stream.of(
+            Block.createCuboidShape(7, 0, 0, 9, 16, 1),
+            Block.createCuboidShape(7, 0, 15, 9, 16, 16),
+            Block.createCuboidShape(2, 0, 2, 13, 10, 13)
+    ).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, BooleanBiFunction.OR)).get();
+    private static final VoxelShape SHAPE_FULL_3_WEST = Stream.of(
+            Block.createCuboidShape(7, 0, 0, 9, 16, 1),
+            Block.createCuboidShape(7, 0, 15, 9, 16, 16),
+            Block.createCuboidShape(7, 15, 1, 9, 16, 15),
+            Block.createCuboidShape(2, 0, 2, 13, 10, 13)
+    ).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, BooleanBiFunction.OR)).get();
+
+    private static final VoxelShape SHAPE_FULL_1_NORTH = Stream.of(
+            Block.createCuboidShape(15, 0, 7, 16, 16, 9),
+            Block.createCuboidShape(2, 0, 2, 13, 10, 13)
+    ).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, BooleanBiFunction.OR)).get();
+    private static final VoxelShape SHAPE_FULL_2_NORTH = Stream.of(
+            Block.createCuboidShape(0, 0, 7, 1, 16, 9),
+            Block.createCuboidShape(15, 0, 7, 16, 16, 9),
+            Block.createCuboidShape(2, 0, 2, 13, 10, 13)
+    ).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, BooleanBiFunction.OR)).get();
+    private static final VoxelShape SHAPE_FULL_3_NORTH = Stream.of(
+            Block.createCuboidShape(0, 0, 7, 1, 16, 9),
+            Block.createCuboidShape(15, 0, 7, 16, 16, 9),
+            Block.createCuboidShape(1, 15, 7, 15, 16, 9),
+            Block.createCuboidShape(2, 0, 2, 13, 10, 13)
+    ).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, BooleanBiFunction.OR)).get();
+
     public CampfireBlock(Settings settings) {
         super(settings);
     }
@@ -66,7 +133,41 @@ public class CampfireBlock extends BlockWithEntity implements BlockEntityProvide
         if (state.get(STAGES) == 1){
             return SHAPE_BASE;
         }
-        return SHAPE_FULL;
+        switch (state.get(COOKING)){
+            case 2 -> {
+                switch (state.get(FACING)){
+                    case Direction.WEST, Direction.EAST -> {
+                        return SHAPE_FULL_1_WEST;
+                    }
+                    default -> {
+                        return SHAPE_FULL_1_NORTH;
+                    }
+                }
+            }
+            case 3 -> {
+                switch (state.get(FACING)){
+                    case Direction.WEST, Direction.EAST -> {
+                        return SHAPE_FULL_2_WEST;
+                    }
+                    default -> {
+                        return SHAPE_FULL_2_NORTH;
+                    }
+                }
+            }
+            case 4 -> {
+                switch (state.get(FACING)){
+                    case Direction.WEST, Direction.EAST -> {
+                        return SHAPE_FULL_3_WEST;
+                    }
+                    default -> {
+                        return SHAPE_FULL_3_NORTH;
+                    }
+                }
+            }
+            default -> {
+                return SHAPE_FULL;
+            }
+        }
     }
 
     @Override
@@ -99,11 +200,41 @@ public class CampfireBlock extends BlockWithEntity implements BlockEntityProvide
     @Override
     protected ActionResult onUseWithItem(ItemStack stack, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
 
+        if (world.getBlockEntity(pos) instanceof CampfireBlockEntity campfireBlockEntity) {
+            if (!stack.isEmpty() && state.get(COOKING) == 4) {
+                for (int i = 0; i < 3; i++) {
+                    if (campfireBlockEntity.getStack(i).isEmpty()) {
+                        campfireBlockEntity.setStack(i, stack.copyWithCount(1));
+                        world.playSound(player, pos, SoundEvents.ENTITY_ITEM_FRAME_ADD_ITEM, SoundCategory.BLOCKS, 1f, 1f);
+                        if (!player.isCreative()) {
+                            stack.decrement(1);
+                        }
+                        world.updateListeners(pos, state, state, 0);
+                        return ActionResult.SUCCESS;
+                    }
+                }
+            }
+            if (stack.isEmpty() && state.get(COOKING) == 4){
+                for (int i = 0; i < 3; i++) {
+                    if (!campfireBlockEntity.getStack(i).isEmpty()){
+                        world.updateListeners(pos, state, state, 0);
+                        world.playSound(player, pos, SoundEvents.ENTITY_ITEM_FRAME_REMOVE_ITEM, SoundCategory.BLOCKS, 1f, 1f);
+                        player.giveOrDropStack(campfireBlockEntity.getStack(i));
+                        campfireBlockEntity.setStack(i, Items.AIR.getDefaultStack());
+                        return ActionResult.SUCCESS;
+                    }
+                }
+            }
+        }
+
+
         if (stack.isOf(Items.STICK) && state.get(STAGES) < 5){
             if (!world.isClient()){
                 world.setBlockState(pos, state.cycle(STAGES));
             }
-            stack.decrement(1);
+            if (!player.isCreative()){
+                stack.decrement(1);
+            }
             return ActionResult.SUCCESS;
         }
         if (stack.isOf(Items.FLINT_AND_STEEL) && state.get(LIT) == 1 && state.get(STAGES) == 5){
@@ -111,35 +242,53 @@ public class CampfireBlock extends BlockWithEntity implements BlockEntityProvide
                 world.setBlockState(pos, state.with(LIT, 2));
             }
             world.playSound(player, pos, SoundEvents.ITEM_FLINTANDSTEEL_USE, SoundCategory.BLOCKS, 1f, 1f);
-            stack.damage(1, player);
+            if (!player.isCreative()){
+                stack.damage(1, player);
+            }
             return ActionResult.SUCCESS;
         }
-        if (stack.isOf(Items.FIRE_CHARGE) && state.get(LIT) == 1 && state.get(STAGES) == 5){
+        if ((stack.isOf(Items.FIRE_CHARGE) || stack.isOf(SnailItems.BURNING_TINDER)) && state.get(LIT) == 1 && state.get(STAGES) == 5){
             if (!world.isClient()){
                 world.setBlockState(pos, state.with(LIT, 2));
             }
             world.playSound(player, pos, SoundEvents.ITEM_FIRECHARGE_USE, SoundCategory.BLOCKS, 1f, 1f);
-            stack.decrement(1);
+            if (!player.isCreative()){
+                stack.decrement(1);
+            }
             return ActionResult.SUCCESS;
         }
-        if (stack.isIn(SnailItemTagsProvider.CAMPFIRE_FUEL) && state.get(LIT) == 2){
+        if (stack.isIn(SnailItemTagsProvider.CAMPFIRE_FUEL) && state.get(LIT) == 2 && !player.isSneaking()){
             if (!world.isClient()) {
                 BlockEntity be = world.getBlockEntity(pos);
                 if (be instanceof CampfireBlockEntity campfireBlockEntity) {
                     campfireBlockEntity.calculateAddedFireTime();
                 }
             }
-            stack.decrement(1);
+            if (!player.isCreative()){
+                stack.decrement(1);
+            }
             return ActionResult.SUCCESS;
         }
-        if (stack.isIn(SnailItemTagsProvider.CAMPFIRE_FUEL) && state.get(LIT) == 3){
+        if (stack.isIn(SnailItemTagsProvider.CAMPFIRE_FUEL) && state.get(LIT) == 3 && !player.isSneaking()){
             if (!world.isClient()) {
                 BlockEntity be = world.getBlockEntity(pos);
                 if (be instanceof CampfireBlockEntity campfireBlockEntity) {
                     campfireBlockEntity.calculateAddedFireTime();
                 }
             }
-            stack.decrement(1);
+            if (!player.isCreative()){
+                stack.decrement(1);
+            }
+            return ActionResult.SUCCESS;
+        }
+
+        if (stack.isOf(Items.STICK) && state.get(STAGES) == 5 && state.get(COOKING) < 4){
+            if (!world.isClient()){
+                world.setBlockState(pos, state.cycle(COOKING));
+            }
+            if (!player.isCreative()){
+                stack.decrement(1);
+            }
             return ActionResult.SUCCESS;
         }
 
@@ -158,6 +307,6 @@ public class CampfireBlock extends BlockWithEntity implements BlockEntityProvide
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(STAGES, LIT);
+        builder.add(STAGES, LIT, COOKING, FACING);
     }
 }
--- src/main/java/net/numericalk/blocks/entity/custom/CampfireBlockEntity.java
+++ src/main/java/net/numericalk/blocks/entity/custom/CampfireBlockEntity.java
@@ -8,21 +8,36 @@ import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.listener.ClientPlayPacketListener;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.s2c.play.BlockEntityUpdateS2CPacket;
+import net.minecraft.recipe.RecipeEntry;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.screen.PropertyDelegate;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.numericalk.blocks.custom.CampfireBlock;
 import net.numericalk.blocks.entity.ImplementedInventory;
 import net.numericalk.blocks.entity.SnailBlockEntities;
+import net.numericalk.items.SnailItems;
+import net.numericalk.recipe.CampfireRecipe;
+import net.numericalk.recipe.CampfireRecipeInput;
+import net.numericalk.recipe.SnailRecipes;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Optional;
 
 public class CampfireBlockEntity extends BlockEntity implements ImplementedInventory {
 
+    private static final int INPUT_SLOT_1 = 0;
+    private static final int INPUT_SLOT_2 = 1;
+    private static final int INPUT_SLOT_3 = 2;
+
+    private int progress = 0;
+    private int maxProgress = 72;
+
     private final float fireDegradeTimeFinal = 1200f;
-    public final float fireMaxTime = 7200f;
     private float fireDegradeTime = fireDegradeTimeFinal;
-    private final DefaultedList<ItemStack> inventory = DefaultedList.ofSize(1, ItemStack.EMPTY);
+    private final DefaultedList<ItemStack> inventory = DefaultedList.ofSize(3, ItemStack.EMPTY);
 
     public CampfireBlockEntity(BlockPos pos, BlockState state) {
         super(SnailBlockEntities.CAMPFIRE_BLOCK_ENTITY, pos, state);
@@ -37,12 +52,14 @@ public class CampfireBlockEntity extends BlockEntity implements ImplementedInven
     protected void writeNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registryLookup) {
         super.writeNbt(nbt, registryLookup);
         Inventories.writeNbt(nbt, inventory, registryLookup);
+        nbt.putFloat("FireDegradeTime", fireDegradeTime);
     }
 
     @Override
     protected void readNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registryLookup) {
         super.readNbt(nbt, registryLookup);
         Inventories.readNbt(nbt, inventory, registryLookup);
+        fireDegradeTime = nbt.getFloat("FireDegradeTime");
     }
 
     @Nullable
@@ -57,29 +74,38 @@ public class CampfireBlockEntity extends BlockEntity implements ImplementedInven
     }
 
     public void tick(World world1, BlockPos pos, BlockState state) {
+
+        if(hasRecipe()) {
+            System.out.println(progress);
+            increaseCraftingProgress();
+            markDirty(world, pos, state);
+
+            if(hasCraftingFinished()) {
+                craftItem();
+                resetProgress();
+            }
+        } else {
+            resetProgress();
+        }
+
+        //FIRE DEGRADING LOGIC
         if (state.get(CampfireBlock.LIT) >= 2 && fireDegradeTime > 0){
             fireDegradeTime --;
-            System.out.println(fireDegradeTime);
         } else if (state.get(CampfireBlock.LIT) == 2 && fireDegradeTime <= 0){
             world1.setBlockState(pos, state.with(CampfireBlock.LIT, 1));
             setFireDegradeTime(fireDegradeTimeFinal);
-            System.out.println(fireDegradeTime);
         }
         if (state.get(CampfireBlock.LIT) == 2 && timeNeededToUpgradeFire(200f)){
             world1.setBlockState(pos, state.with(CampfireBlock.LIT, 3));
-            System.out.println(fireDegradeTime);
         }
         if (state.get(CampfireBlock.LIT) == 3 && fireDegradeTime <= fireDegradeTimeFinal){
             world1.setBlockState(pos, state.with(CampfireBlock.LIT, 2));
-            System.out.println(fireDegradeTime);
         }
         if (state.get(CampfireBlock.LIT) == 3 && timeNeededToUpgradeFire(400f)){
             world1.setBlockState(pos, state.with(CampfireBlock.LIT, 4));
-            System.out.println(fireDegradeTime);
         }
         if (state.get(CampfireBlock.LIT) == 4 && fireDegradeTime <= fireDegradeTimeFinal * 3){
             world1.setBlockState(pos, state.with(CampfireBlock.LIT, 3));
-            System.out.println(fireDegradeTime);
         }
     }
 
@@ -97,5 +123,73 @@ public class CampfireBlockEntity extends BlockEntity implements ImplementedInven
 
     public void setFireDegradeTime(float fireDegradeTime) {
         this.fireDegradeTime = fireDegradeTime;
+    }
+    private void resetProgress() {
+        this.progress = 0;
+        this.maxProgress = 72;
+    }
+    private void increaseCraftingProgress() {
+        this.progress++;
+    }
+
+    private void craftItem() {
+        Optional<RecipeEntry<CampfireRecipe>> recipe = getCurrentRecipe();
+
+        ItemStack output = recipe.get().value().output();
+        for (int i = 0; i < 3; i++){
+            this.removeStack(i, 1);
+            this.setStack(i, new ItemStack(output.getItem(),
+                    this.getStack(i).getCount() + output.getCount()));
+        }
+    }
+    private boolean hasCraftingFinished() {
+        return this.progress >= this.maxProgress;
+    }
+    private boolean hasRecipe() {
+        Optional<RecipeEntry<CampfireRecipe>> recipe = getCurrentRecipe();
+        if (recipe.isEmpty()){
+            return false;
+        }
+
+        ItemStack output = recipe.get().value().output();
+
+        return canInsertAmountIntoOutputSlot(output.getCount()) && canInsertItemIntoOutputSlot(output);
+    }
+
+    private Optional<RecipeEntry<CampfireRecipe>> getCurrentRecipe() {
+        for (int i = 0; i < inventory.size(); i++) {
+            ItemStack stack = this.getStack(i);
+            if (!stack.isEmpty()) {
+                Optional<RecipeEntry<CampfireRecipe>> recipe = ((ServerWorld) this.getWorld())
+                        .getRecipeManager()
+                        .getFirstMatch(SnailRecipes.CAMPFIRE_TYPE, new CampfireRecipeInput(stack), this.getWorld());
+
+                if (recipe.isPresent()) {
+                    return recipe;
+                }
+            }
+        }
+        return Optional.empty();
+    }
+
+    private boolean canInsertAmountIntoOutputSlot(int count) {
+        for (int i = 0; i < 3; i++) {
+            int maxCount = this.getStack(i).isEmpty() ? 64 : this.getStack(i).getMaxCount();
+            int currentCount = this.getStack(i).getCount();
+
+            if (maxCount >= currentCount + count){
+                return true;
+            }
+        }
+
+        return false;
+    }
+    private boolean canInsertItemIntoOutputSlot(ItemStack output) {
+        for (int i = 0; i < 3; i++){
+            if (this.getStack(i).isEmpty() || this.getStack(i).getItem() == output.getItem()){
+                return true;
+            }
+        }
+        return false;
     }
 }

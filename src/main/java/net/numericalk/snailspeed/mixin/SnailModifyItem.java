a647cce23a071a473e160cb3c21d416581e4c08c
Fungus Defender <capybara.okipullup121@gmail.com>
2025-07-11 20:30:12 +0700

    Reverted Flint Stack Count To 64

--- src/main/java/net/numericalk/snailspeed/mixin/SnailModifyItem.java
+++ src/main/java/net/numericalk/snailspeed/mixin/SnailModifyItem.java
@@ -1,12 +1,15 @@
 package net.numericalk.snailspeed.mixin;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.util.Identifier;
+import net.numericalk.snailspeed.datagen.SnailBlockTagsProvider;
 import net.numericalk.snailspeed.utils.SnailToolMaterial;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -19,41 +22,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.function.Function;
 
-@Mixin(Items.class)
+@Mixin(Item.class)
 public abstract class SnailModifyItem {
-
-    @Redirect(
-            method = "<clinit>",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/item/Items;register(Ljava/lang/String;)Lnet/minecraft/item/Item;"
-            )
-    )
-    private static Item redirectRegister(String id) {
-        if (id.equals("flint")) {
-            return register(id, settings ->
-                    new PickaxeItem(SnailToolMaterial.FLINT_MATERIAL, 1, -2.8f, settings.maxCount(1))
-            );
+    @Inject(method = "getMiningSpeed", at = @At("HEAD"), cancellable = true)
+    private void modifyMiningSpeed(ItemStack stack, BlockState state, CallbackInfoReturnable<Float> cir) {
+        if ((Object)this == Items.FLINT) {
+            if (state.isIn(BlockTags.STONE_ORE_REPLACEABLES) || state.isIn(SnailBlockTagsProvider.STONE_BLOCK)) {
+                cir.setReturnValue(3.0F);
+            }
         }
-        return Items.register(id);
-    }
-
-    @Unique
-    private static Item register(String id, Function<Item.Settings, Item> factory) {
-        return register(keyOf(id), factory, new Item.Settings());
-    }
-
-    @Unique
-    private static Item register(RegistryKey<Item> key, Function<Item.Settings, Item> factory, Item.Settings settings) {
-        Item item = factory.apply(settings.registryKey(key));
-        if (item instanceof BlockItem blockItem) {
-            blockItem.appendBlocks(Item.BLOCK_ITEMS, item);
-        }
-        return Registry.register(Registries.ITEM, key, item);
-    }
-
-    @Unique
-    private static RegistryKey<Item> keyOf(String id) {
-        return RegistryKey.of(RegistryKeys.ITEM, Identifier.ofVanilla(id));
     }
 }
